f5134d50c451cb7d4067b6ffe4d07da11b22c215
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-05-28 16:06:22 -0400

    added all the functionality needed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,26 @@
-alert("connected with JavaScript");
+//check off specific item by clicking//
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("done");
+});
+
+//click on X to delete an item//
+$("ul").on("click", "span", function(event){
+    //removing an item with fadeout effect
+     $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+     });
+    //to stop the code bubble//
+    event.stopPropagation();
+});
+
+//adding new item//
+$("input[type = 'text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing inut as new item//
+        var newToDo = $(this).val();
+        //clear the input//
+        $(this).val("");
+        //create a new li and add to ul//
+        $("ul").append("<li><span>X <span>" + newToDo + "</li>")
+    }
+});
